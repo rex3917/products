4024113d583ff7fba6d0a8ff9258639a4c49efc2
rex3917 <louis3917@gmail.com>
2022-10-28 22:41:34 +0800

    add check file

--- product.py
+++ product.py
@@ -1,4 +1,25 @@
+# 讀取檔案
+# split(x)切割x
+# strip處理換行符號
+# 現在Excel不需要encoding 'utf-8'也可開啟
+# if os.path.isfile檢查檔案是否存在(作業系統模組)
+
+import os # operating system
+# 讀取檔案
 products = []
+if os.path.isfile('products.csv'):
+    print('Yaeh 找到檔案了!')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案')
+
+# 讓使用者輸入新的商品
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -7,9 +28,11 @@ while True:
     products.append([name, price])
 print(products)
 
+# 印出所有的購買紀錄
 for p in products:
     print(p[0], '的價格是', p[1])
 
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,價格\n')
     for p in products:
