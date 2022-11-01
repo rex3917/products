6ac133016e9f5320c7201bf3dcfae68372c28ac8
rex3917 <louis3917@gmail.com>
2022-11-01 19:30:57 +0800

    add function

--- product.py
+++ product.py
@@ -6,34 +6,44 @@
 
 import os # operating system
 # 讀取檔案
-products = []
-if os.path.isfile('products.csv'):
-    print('Yaeh 找到檔案了!')
-    with open('products.csv', 'r', encoding='utf-8') as f:
-        for line in f:
-            if '商品,價格' in line:
-                continue
-            name, price = line.strip().split(',')
-            products.append([name, price])
-    print(products)
-else:
-    print('找不到檔案')
-
+def read_file(filename):
+    products = []
+    if os.path.isfile(filename):
+        print('Yaeh 找到檔案了!')
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                if '商品,價格' in line:
+                    continue
+                name, price = line.strip().split(',')
+                products.append([name, price])
+        print(products)
+    else:
+        print('找不到檔案')
+    return products
 # 讓使用者輸入新的商品
-while True:
-    name = input('請輸入商品名稱: ')
-    if name == 'q':
-        break
-    price = int(input('請輸入商品價格: '))
-    products.append([name, price])
-print(products)
+def user_input(products):
+    while True:
+        name = input('請輸入商品名稱: ')
+        if name == 'q':
+            break
+        price = int(input('請輸入商品價格: '))
+        products.append([name, price])
+    print(products)
+    return products
 
 # 印出所有的購買紀錄
-for p in products:
-    print(p[0], '的價格是', p[1])
+def print_products(products):
+    for p in products:
+        print(p[0], '的價格是', p[1])
 
 # 寫入檔案
-with open('products.csv', 'w', encoding='utf-8') as f:
-    f.write('商品,價格\n')
-    for p in products:
-        f.write(str(p[0]) + ',' + str(p[1]) + '\n')
+def write_file(products):
+    with open('products.csv', 'w', encoding='utf-8') as f:
+        f.write('商品,價格\n')
+        for p in products:
+            f.write(str(p[0]) + ',' + str(p[1]) + '\n')
+
+products = read_file('products.csv')
+products = user_input(products)
+print_products(products)
+write_file(products)
